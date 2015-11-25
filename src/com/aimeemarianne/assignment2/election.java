05ed224f52f7f2a1d5c4a4a0d7d5ef84f3ec2eb3
mazmills1975 <mazmaz37@hotmail.co.uk>
2015-11-25 11:23:47 +0000

    Ive updated xyz

--- src/com/aimeemarianne/assignment2/election.java
+++ src/com/aimeemarianne/assignment2/election.java
@@ -5,7 +5,13 @@ package com.aimeemarianne.assignment2;
  */
 public class election {
 
-    public static void main(String[] args){}
+    public static void main(String[] args){
+
+
+        //jjkijiouhiojil
+
+        
+    }
 
 
 
