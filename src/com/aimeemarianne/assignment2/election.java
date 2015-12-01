8e0015ade1162e78d26ca30ac7423e8f0e0838c6
aimeem1991 <aimee.m1991@gmailcom>
2015-12-01 14:38:35 +0000

    updated

--- src/com/aimeemarianne/assignment2/election.java
+++ src/com/aimeemarianne/assignment2/election.java
@@ -1,5 +1,7 @@
 package com.aimeemarianne.assignment2;
 
+import javax.swing.*;
+
 /**
  * Created by mur07114879 on 24/11/2015.
  */
@@ -7,9 +9,44 @@ public class election {
 
     public static void main(String[] args){
 
+        JFrame frame = new JFrame ("President Election Login");
+        frame.setSize (300, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        layout(panel);
+        frame.setVisible(true);
+
+
+private static void layout(JPanel layout) {
+
+        layout.setLayout(null);
+
+
+        JLabel username = new JLabel("Username");
+        username.setBounds(10, 10, 100, 25);
+        layout.add(username);
+        JTextField utext = new JTextField(40);
+        utext.setBounds(10 ,10, 100, 25);
+        layout.add(utext);
+
+        JLabel password = new JLabel("Password");
+        password.setBounds(10, 10, 100, 25);
+        layout.add(password);
+        JTextField ptext = new JTextField(40);
+        ptext.setBounds(10, 10, 100, 25);
+        layout.add(ptext);
+
+        JButton login = new JButton("Login");
+        login.setBounds(10, 80, 80, 25);
+        layout.add(login);
+
+
+        BoxLayout boxlayout = new BoxLayout(layout.getContentPane(), BoxLayout.Y_AXIS);
+        layout.setLayout(boxlayout);
     }
+
 
 
 
